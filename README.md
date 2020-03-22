ce42852d1abf468204220e7fcc10221268eeb688
bryantran3562 <bryantran3562@gmail.com>
2020-03-22 15:55:00 -0500

    vuetify-card

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 //BT - You can over ride the theme color default by your own below. Look in plugins/vuetify.js
+
+Team - with card.
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="team">
-    <h1 class="subtitle-1 grey--text">Team</h1>
+        <h1 class="subtitle-1 grey--text">Team</h1>
       <v-container fluid class="my-5">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor illum ipsa asperiores tempore! Vitae impedit deserunt eum id voluptatem nihil libero voluptas. Ratione fugiat odio eligendi quasi, voluptatibus quisquam!</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil obcaecati iusto aut dolor id, aliquam nobis distinctio et vitae aspernatur culpa velit accusantium. Labore, amet ea at impedit quam voluptatem.</p>
+            <v-layout row wrap>
+            <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+            <v-card text class="text-xs-center ma-3">
+                <v-responsive class="pt-4">
+                <v-avatar size="100" class="grey lighten-2">
+                    <img :src="person.avatar" />
+                </v-avatar>
+                </v-responsive>
+                <v-card-text>
+                <div class="subheading">{{ person.name }}</div>
+                <div class="grey--text">{{ person.role }}</div>
+                </v-card-text>
+                <v-card-actions>
+                <v-btn text color="grey">
+                    <v-icon small left>mdi-message</v-icon>
+                    <span class="">Message</span>
+                </v-btn>
+                </v-card-actions>
+            </v-card>
+            </v-flex>
+        </v-layout>
       </v-container>
   </div>
 </template>
@@ -13,8 +32,16 @@
 
 export default {
 
-  components: {
-
+  data() {
+    return {
+      team: [
+        { name: 'The Net Ninja', role: 'Web developer', avatar: '/avatar-1.png' },
+        { name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png' },
+        { name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png' },
+        { name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png' },
+        { name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png'}
+      ]
+    }
   }
 }
 </script>
