380ddffbb4285687f5280f0346589f27f1315f32
bryantran3562 <bryantran3562@gmail.com>
2020-03-22 00:17:17 -0500

    vuetify-list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,16 @@
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer" class="primary">
-      <p>test</p>
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -27,6 +36,11 @@ export default {
   data() {
     return {
         drawer: false,
+        links: [
+            { icon: 'mdi-monitor-dashboard', text: 'Dashboard', route: '/' },
+            { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+            { icon: 'mdi-account', text: 'Team', route: '/team' },
+        ]
     }
   }
 }
