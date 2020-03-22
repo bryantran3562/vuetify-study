7caf8b27c1b25c67d93eafa9401f47a66a21f1b8
bryantran3562 <bryantran3562@gmail.com>
2020-03-21 22:19:04 -0500

    vuetify-icon-button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="home">
-    <h1>This is Home Pge</h1>
+    <h1>This is Home Page</h1>
+
+    <!-- BT - Button -->
+
+    <v-btn class="lime black--text">Click Me</v-btn>
+    <p></p>
+
+    <!-- BT - Instead of using class. You can also use Props to control the color of the button -->
+    <v-btn color="lime">Click Me</v-btn>
+    <p></p>
+
+    <!-- BT - Icons
+    Notes: Because the email it too close to the Email me text - We can give it a props - right to have
+           the email icon move to the right.
+     -->
+
+    <v-btn depressed class="primary">
+      <span>Email Me standard</span>
+      <v-icon right>mdi-email</v-icon>
+    </v-btn>
+    <p></p>
+
+    <v-btn depressed large class="success">
+      <v-icon large left>mdi-email</v-icon>
+      <span>Email Me large</span>
+    </v-btn>
+    <p></p>
+
+    <v-btn depressed small class="error">
+      Email Me small
+      <v-icon right small="">mdi-email</v-icon>
+    </v-btn>
+
+    <p></p>
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
@@ -14,3 +51,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+</style>
