a624f5786b17ed40ebe39f83db3ea61502108c56
bryantran3562 <bryantran3562@gmail.com>
2020-03-22 02:03:02 -0500

    vuetify-grid-system-2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,31 +3,57 @@
     <h1 class="subtitle-1 grey--text">Dash Board</h1>
 
       <v-container fluid class="my-5">
-          <v-row row wrap>
-            <v-col cols="12" sm="12" md="6">
-              <v-btn outline block class="primary">1</v-btn>
-            </v-col>
-            <v-col cols="12" sm="4" md="2">
-              <v-btn outline block class="primary">2</v-btn>
-            </v-col>
-            <v-col cols="12" sm="4" md="2">
-              <v-btn outline block class="primary">3</v-btn>
-            </v-col>
-            <v-col cols="12" sm="4" md="2">
-              <v-btn outline block class="primary">4</v-btn>
-            </v-col>
-          </v-row>
-
-          <!-- <v-row row wrap justify-space-around>
-            <v-col xs4 md3>
-              <v-btn outline block class="success">1</v-btn>
-            </v-col>
-            <v-col xs4 md3>
-              <v-btn outline block class="success">2</v-btn>
-            </v-col>
-          </v-row> -->
+        <v-card flat v-for="project in projects" :key="project.title">
+          <!-- BT - Using syntax ES6 backticks to get the outcom. -->
+          <v-layout row wrap :class="`pa-3 project ${project.status}`">
+            <v-flex xs12 md6>
+              <div class="caption grey--text">Project title</div>
+              <div>{{ project.title }}</div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">Person</div>
+              <div>{{ project.person }}</div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">Due by</div>
+              <div>{{ project.due }}</div>
+            </v-flex>
+            <v-flex xs2 sm4 md2>
+              <div class="caption grey--text">Status</div>
+              <div>{{ project.status }}</div>
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+        </v-card>
 
       </v-container>
 
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+</style>
