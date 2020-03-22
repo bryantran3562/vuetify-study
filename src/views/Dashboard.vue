e603ceea2582aabcfbe02844a02ecc9dd5c4127b
bryantran3562 <bryantran3562@gmail.com>
2020-03-22 01:46:07 -0500

    vuetify-grid-system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,8 +3,30 @@
     <h1 class="subtitle-1 grey--text">Dash Board</h1>
 
       <v-container fluid class="my-5">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor illum ipsa asperiores tempore! Vitae impedit deserunt eum id voluptatem nihil libero voluptas. Ratione fugiat odio eligendi quasi, voluptatibus quisquam!</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil obcaecati iusto aut dolor id, aliquam nobis distinctio et vitae aspernatur culpa velit accusantium. Labore, amet ea at impedit quam voluptatem.</p>
+          <v-row row wrap>
+            <v-col cols="12" sm="12" md="6">
+              <v-btn outline block class="primary">1</v-btn>
+            </v-col>
+            <v-col cols="12" sm="4" md="2">
+              <v-btn outline block class="primary">2</v-btn>
+            </v-col>
+            <v-col cols="12" sm="4" md="2">
+              <v-btn outline block class="primary">3</v-btn>
+            </v-col>
+            <v-col cols="12" sm="4" md="2">
+              <v-btn outline block class="primary">4</v-btn>
+            </v-col>
+          </v-row>
+
+          <!-- <v-row row wrap justify-space-around>
+            <v-col xs4 md3>
+              <v-btn outline block class="success">1</v-btn>
+            </v-col>
+            <v-col xs4 md3>
+              <v-btn outline block class="success">2</v-btn>
+            </v-col>
+          </v-row> -->
+
       </v-container>
 
   </div>
